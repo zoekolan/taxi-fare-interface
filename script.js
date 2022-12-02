d0949e2be3394ace45d899e9c35f09dd3e37d7a1
zoe kolan <zoe.kolan2@gmail.com>
2022-12-02 11:26:48 +0100

    tried

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let taxiFareApiUrl = 'http://localhost:8001/predict'; // replace with your API endpoint
+let taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict'; // replace with your API endpoint
+//const taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
